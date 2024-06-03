c39b8207b345580593f0bfbc72a0650f3d44f6d7
hy <thanh.doan@skymavis.com>
2024-06-03 15:19:09 +0700

    fix: new client ID

--- apps/playground/src/connectors/MavisIdConnector.tsx
+++ apps/playground/src/connectors/MavisIdConnector.tsx
@@ -38,7 +38,7 @@ class MavisIdConnector extends BaseConnector<MavisIdProvider> {
 
   async connect(chainId: number): Promise<IConnectResult<MavisIdProvider>> {
     const newProvider = MavisIdProvider.create({
-      clientId: "xdemo",
+      clientId: "0e188f93-b419-4b0f-8df4-0f976da91ee6",
       chainId: chainId,
     })
 
