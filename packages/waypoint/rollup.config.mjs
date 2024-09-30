5ba9781b6d4f8ab8833690de74b51e6c689bc6e0
Nguyen Huu Gia Tri <44458003+nguyenhuugiatri@users.noreply.github.com>
2024-09-30 14:53:35 +0700

    fix: module conflict on Nextjs/Remix (#37)

--- packages/waypoint/rollup.config.mjs
+++ packages/waypoint/rollup.config.mjs
@@ -1,19 +1,29 @@
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
 import { babel } from "@rollup/plugin-babel"
 import commonjs from "@rollup/plugin-commonjs"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
+import { globSync } from "glob"
 import { defineConfig } from "rollup"
 import peerDepsExternal from "rollup-plugin-peer-deps-external"
 import nodePolyfills from "rollup-plugin-polyfill-node"
 import typescript from "rollup-plugin-typescript2"
 
 const mainConfig = defineConfig({
-  input: ["src/index.ts"],
+  // This configuration the same as preserveModules but without node_modules folder
+  // To solve module conflict on Nextjs/Remix
+  // Ref: https://rollupjs.org/configuration-options/#input
+  input: Object.fromEntries(
+    globSync("src/**/*.ts").map(file => [
+      path.relative("src", file.slice(0, file.length - path.extname(file).length)),
+      fileURLToPath(new URL(file, import.meta.url)),
+    ]),
+  ),
   output: [
     {
       dir: "dist",
       format: "esm",
-      preserveModules: true,
-      preserveModulesRoot: "src",
     },
   ],
   plugins: [
