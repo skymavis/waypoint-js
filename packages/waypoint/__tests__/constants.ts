9e3f442899b38d9df9454968c02e7a5684ae66d7
nigel <nghia.thanh.nguyen@skymavis.com>
2024-11-10 16:07:28 +0700

    unit test for waypoint sdk web core

--- packages/waypoint/__tests__/constants.ts
+++ packages/waypoint/__tests__/constants.ts
@@ -2,4 +2,6 @@ export const CONFIG = {
   WAYPOINT_ORIGIN: "https://id.skymavis.one",
   CLIENT_ID: "xdemo",
   REDIRECT_URL: "https://xdemo.vercel.app",
+  CHAIN_ID: 2021,
+  EXPECTED_ADDRESS: "0x0409e230c2F24db1ff119C3A4681aD884A38D646",
 }
