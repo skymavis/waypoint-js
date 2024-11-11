c06ffa70361cc0bbf738f3432dada10fc583bb89
nigel <nghia.thanh.nguyen@skymavis.com>
2024-11-11 10:45:42 +0700

    unit testing for waypoint sdk web utils

---
+++ packages/waypoint/__tests__/web/utils/popup.test.ts
@@ -0,0 +1,74 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest"
+
+import { openPopup, replaceUrl } from "./../../../web/utils/popup"
+
+describe("Popup and URL functions", () => {
+  beforeEach(() => {
+    // Mock screen properties
+    globalThis.window.screenLeft = 0
+    globalThis.window.screenTop = 0
+    globalThis.window.innerWidth = 1024
+    globalThis.window.innerHeight = 768
+
+    globalThis.window.open = vi.fn(() => {
+      const _window = window
+      _window.focus = vi.fn()
+      return _window
+    })
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  describe("openPopup", () => {
+    test("includes query parameters in the popup URL", () => {
+      const mockUrl = "https://example.com"
+      const queryParams = { foo: "bar", baz: "123" }
+      openPopup(mockUrl, queryParams)
+
+      const url = new URL(mockUrl)
+      url.searchParams.set("foo", "bar")
+      url.searchParams.set("baz", "123")
+
+      expect(globalThis.window.open).toHaveBeenCalledWith(
+        url,
+        "_blank",
+        `
+        scrollbars=yes,
+        width=480,
+        height=720,
+        top=24,
+        left=272
+      `,
+      )
+    })
+
+    test("throws UserRejectedRequestError if popup is blocked", () => {
+      vi.spyOn(globalThis.window, "open").mockImplementation(() => null)
+
+      expect(() => openPopup("https://example.com")).toThrowError(
+        "Popup window is BLOCKED by the browser",
+      )
+    })
+  })
+
+  describe("replaceUrl", () => {
+    test("replaces the current URL with provided URL and query params", () => {
+      Object.defineProperty(window, "location", {
+        value: { assign: vi.fn() },
+        writable: true,
+      })
+
+      const mockUrl = "https://example.com"
+      const queryParams = { foo: "bar", baz: 123 }
+
+      replaceUrl(mockUrl, queryParams)
+
+      const url = new URL(mockUrl)
+      url.searchParams.set("foo", "bar")
+      url.searchParams.set("baz", "123")
+      expect(window.location.assign).toBeCalledWith(url)
+    })
+  })
+})
---
+++ packages/waypoint/__tests__/web/utils/storage.test.ts
@@ -0,0 +1,87 @@
+// storage.test.ts
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest"
+
+import {
+  getStorage,
+  removeStorage,
+  setStorage,
+  STORAGE_ADDRESS_KEY,
+  STORAGE_PREFIX,
+} from "../../../web/utils/storage"
+
+describe("Storage functions", () => {
+  // Mocking localStorage before each test
+  beforeEach(() => {
+    globalThis.localStorage = {
+      getItem: vi.fn(),
+      setItem: vi.fn(),
+      removeItem: vi.fn(),
+      clear: vi.fn(),
+      key: vi.fn(),
+      length: 6,
+    }
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  test("should define constants for STORAGE_ADDRESS_KEY and STORAGE_PREFIX", () => {
+    expect(STORAGE_ADDRESS_KEY).toBe("ADDRESS")
+    expect(STORAGE_PREFIX).toBe("RONIN.WAYPOINT")
+  })
+
+  test("should get the correct value from localStorage when available", () => {
+    const mockValue = "some-address-value"
+    vi.spyOn(window.localStorage, "getItem").mockImplementation(() => mockValue)
+
+    const result = getStorage(STORAGE_ADDRESS_KEY)
+
+    expect(globalThis.localStorage.getItem).toHaveBeenCalledWith(
+      `${STORAGE_PREFIX}:${STORAGE_ADDRESS_KEY}`,
+    )
+    expect(result).toBe(mockValue)
+  })
+
+  test("should return undefined when item is not found in localStorage", () => {
+    globalThis.localStorage.getItem = vi.fn().mockReturnValue(null)
+
+    const result = getStorage(STORAGE_ADDRESS_KEY)
+
+    expect(result).toBeNull()
+    expect(globalThis.localStorage.getItem).toHaveBeenCalledWith(
+      `${STORAGE_PREFIX}:${STORAGE_ADDRESS_KEY}`,
+    )
+  })
+
+  test("should set the correct value in localStorage", () => {
+    const mockValue = "some-address-value"
+    setStorage(STORAGE_ADDRESS_KEY, mockValue)
+
+    expect(globalThis.localStorage.setItem).toHaveBeenCalledWith(
+      `${STORAGE_PREFIX}:${STORAGE_ADDRESS_KEY}`,
+      mockValue,
+    )
+  })
+
+  test("should remove the correct item from localStorage", () => {
+    removeStorage(STORAGE_ADDRESS_KEY)
+
+    expect(globalThis.localStorage.removeItem).toHaveBeenCalledWith(
+      `${STORAGE_PREFIX}:${STORAGE_ADDRESS_KEY}`,
+    )
+  })
+
+  test("should return false if localStorage is not available", () => {
+    delete (globalThis.window as { localStorage?: Storage })["localStorage"]
+
+    const result = getStorage(STORAGE_ADDRESS_KEY)
+    expect(result).toBe(false)
+
+    const setStorageResult = setStorage(STORAGE_ADDRESS_KEY, "test-value")
+    expect(setStorageResult).toBe(false)
+
+    const removeStorageResult = removeStorage(STORAGE_ADDRESS_KEY)
+    expect(removeStorageResult).toBe(false)
+  })
+})
